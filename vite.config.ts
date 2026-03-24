b42fc7d47b94a5ba0a3d18f81c5b01e1d28c2683
chris21million <chris21million@proton.me>
2026-03-24 00:14:33 +0000

    test1

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/lightning-chess-folder-reorg/",
+  base: "/lightning-chess/",
   plugins: [react()],
 })
